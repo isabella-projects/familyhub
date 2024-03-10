a9bf97b9c2762ad61b27b28bc3abc47044f96871
isabella-projects <damyan4oy@gmail.com>
2024-03-10 15:02:28 +0100

    Small refactor v2

--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -1,7 +1,7 @@
-@if ($isAdmin)
+@if (auth()->user()->isAdmin)
 <span class="text-danger role" title="Administrator" data-toggle="tooltip" data-placement="top"><i
-        class="fas fa-user-shield fa-xs"></i></span>
+                class="fas fa-user-shield fa-xs"></i></span>
 @else
 <span class="text-primary role" title="User" data-toggle="tooltip" data-placement="top"><i
-        class="fas fa-user-alt fa-xs"></i></span>
+                class="fas fa-user-alt fa-xs"></i></span>
 @endif
